bcb83bc47dd7481f80cca0c939b67e5acb994e90
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-09 17:45:27 -0400

    Generate devise views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,15 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, except: %i[index]
+    
     def index
         @events = Event.all
     end
 
+    def new
+      @event = current_user.events.build
+    end
+
+    def create
+      @event = current_user.events.build(event_params)
+    end
 end

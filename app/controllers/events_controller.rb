4550cebfaa152c7781321f4637a7de43d64f0da5
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-10 17:21:31 -0400

    Add date & time fields to new Event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,15 +6,25 @@ class EventsController < ApplicationController
     end
 
     def new
-      @event = current_user.events.build
+      @event = Event.new
     end
 
     def create
       @event = current_user.events.build(event_params)
+
+      respond_to do |format|
+        if @event.save
+          format.html { redirect_to events_url, notice: 'Event was successfully created.' }
+          format.json { render :show, status: :created, location: @event }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     private
     def event_params
-      params.require(:event).permit(:title, :location, :date, :time)
+      params.require(:event).permit(:title, :location, :date, :time, :creator)
     end
 end

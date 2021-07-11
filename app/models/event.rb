ca4d9412532a97780e42e390eb2f5291ef057c26
petumazo <davidalvarezmazzo@gmail.com>
2021-07-10 20:32:41 -0500

    Create invitation model and add assosiation to user, event and ivitation models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
+  has_many :invitations, foreign_key: :attended_event_id
+  has_many :attendees, through: :invitations, source: :attendee
   belongs_to :user, class_name: "User"
 end

d3761a5fe540174c3f3a587467917aa0d3fd6f93
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-07-12 13:47:12 -0400

    Correct rubocop linter errors

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,5 @@
 class InvitationsController < ApplicationController
-def create
+  def create
     event = Event.find(params[:event])
     confirmation = event.invitations.build(attendee_id: current_user.id)
     if confirmation.save

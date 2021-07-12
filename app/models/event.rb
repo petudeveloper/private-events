36675a2c0e15c12fc06c15c986a9cb54a3bbcc99
petumazo <davidalvarezmazzo@gmail.com>
2021-07-12 12:37:45 -0500

    Change Event column from user_id to creator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations, source: :attendee
-  belongs_to :user, class_name: 'User'
+  belongs_to :creator, class_name: 'User'
   
   scope :past_events, -> { where('date < ?', DateTime.now) }
   scope :future_events, -> { where('date > ?', DateTime.now) }

9a8798736bc24aa0b92b44d8bb08f2aebd9a4786
petumazo <davidalvarezmazzo@gmail.com>
2021-07-13 12:35:47 -0500

    Correct rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
-    if user_signed_in?
-      @user = current_user
-    else
-    @user = User.find(params[:id])
-    end
+    @user = if user_signed_in?
+              current_user
+            else
+              User.find(params[:id])
+            end
     @created_events = @user.events
     @past_events = current_user.events.past_events
     @upcoming_events = current_user.events.future_events
